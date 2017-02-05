27e2cfa8c92121b9ef671f26052cf446c814ab43
Andrea Benetti <andrea.benetti.dev@gmail.com>
2017-02-05 11:28:34 +0100

    twig first commit

---
+++ src/main/java/be/andreadev/enterprisemgt/js/controller/TwigSampleController.java
@@ -0,0 +1,24 @@
+package be.andreadev.enterprisemgt.js.controller;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@EnableAutoConfiguration
+public class TwigSampleController {
+
+	@RequestMapping("/twig/helloworld/{name}")
+    public String indexAction(ModelMap model, @PathVariable("name") String name) {
+        model.addAttribute("name", name);
+        return "index";
+    }
+	
+	public static void main(String[] args) {
+		SpringApplication.run(TwigSampleController.class, args);
+	}
+
+}
